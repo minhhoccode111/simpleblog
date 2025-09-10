aeaebdb3f0288e79d6724b645021da7524e127ae
minhhoccode111 <minhhoccode111@gmail.com>
2025-09-10 21:43:46 +0700

    add: routers setup

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -8,64 +8,29 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func (s *Server) RegisterRoutes() http.Handler {
-	r := mux.NewRouter()
-	r.Use(s.corsMiddleware)
-	r.Use(s.basicAuthentication)
-
-	r.HandleFunc("/", s.HelloWorldHandler)
-
-	return r
-}
-
-func (s *Server) corsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Set CORS headers
-		w.Header().
-			Set("Access-Control-Allow-Origin", "*")
-			// Replace "*" with specific origins if needed
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
-		w.Header().
-			Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, X-CSRF-Token")
-		w.Header().
-			Set("Access-Control-Allow-Credentials", "false")
-			// Set to "true" if credentials are required
-
-		// Handle preflight OPTIONS requests
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
-
-		// Proceed with the next handler
-		next.ServeHTTP(w, r)
-	})
+func unauthorizedResponse(w http.ResponseWriter) {
+	w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+	http.Error(w, "Unauthorized", http.StatusUnauthorized)
 }
 
 func (s *Server) basicAuthentication(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// first request will have no authorization header
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
-			w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
-			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			unauthorizedResponse(w)
 			return
 		}
 
-		// remove prefix "Authorization: Basic " from header
 		encodedBase64 := authHeader[len("Basic "):]
-
 		payload, err := base64.StdEncoding.DecodeString(encodedBase64)
 		if err != nil {
-			w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
-			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			unauthorizedResponse(w)
 			return
 		}
 
 		parts := strings.SplitN(string(payload), ":", 2)
 		if parts[0] != "admin" || parts[1] != "admin" {
-			w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
-			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			unauthorizedResponse(w)
 			return
 		}
 
@@ -73,23 +38,39 @@ func (s *Server) basicAuthentication(next http.Handler) http.Handler {
 	})
 }
 
-func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("hello, world"))
+// public
+func (s *Server) IndexHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/articles", http.StatusFound)
 }
+func (s *Server) GetAllPublishedArticlesHandler(w http.ResponseWriter, r *http.Request) {}
+func (s *Server) GetPublishedArticleHandler(w http.ResponseWriter, r *http.Request)     {}
 
-func (s *Server) HomeHandler(w http.ResponseWriter, r *http.Request)           {}
-func (s *Server) AboutHandler(w http.ResponseWriter, r *http.Request)          {}
-func (s *Server) GetAllArticlesHandler(w http.ResponseWriter, r *http.Request) {}
-func (s *Server) GetArticleHandler(w http.ResponseWriter, r *http.Request)     {}
-
+// auth
 func (s *Server) AdminIndexHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/admin/articles", http.StatusFound)
 }
+func (s *Server) AdminGetAllArticlesHandler(w http.ResponseWriter, r *http.Request)   {}
+func (s *Server) AdminCreateArticleHandler(w http.ResponseWriter, r *http.Request)    {}
+func (s *Server) AdminUpdateArticleGetHandler(w http.ResponseWriter, r *http.Request) {}
+func (s *Server) AdminUpdateArticleHandler(w http.ResponseWriter, r *http.Request)    {}
+func (s *Server) AdminDeleteArticleHandler(w http.ResponseWriter, r *http.Request)    {}
 
-func (s *Server) AdminGetAllArticlesHandler(w http.ResponseWriter, r *http.Request) {}
-func (s *Server) AdminCreateArticleHandler(w http.ResponseWriter, r *http.Request)  {}
-func (s *Server) AdminGetArticleHandler(w http.ResponseWriter, r *http.Request)     {}
-func (s *Server) AdminEditArticleHandler(w http.ResponseWriter, r *http.Request)    {}
-func (s *Server) AdminDeleteArticleHandler(w http.ResponseWriter, r *http.Request)  {}
+func (s *Server) RegisterRoutes() http.Handler {
+	r := mux.NewRouter()
+	r.HandleFunc("/", s.IndexHandler).Methods("GET")
+	r.HandleFunc("/articles", s.GetAllPublishedArticlesHandler).Methods("GET")
+	r.HandleFunc("/articles/{slug}", s.GetPublishedArticleHandler).Methods("GET")
+
+	r.Use(s.basicAuthentication)
+	r.HandleFunc("/admin", s.AdminIndexHandler).Methods("GET")
+	// with ?action=new
+	r.HandleFunc("/admin/articles", s.AdminGetAllArticlesHandler).Methods("GET")
+	r.HandleFunc("/admin/articles", s.AdminCreateArticleHandler).Methods("POST")
+
+	// with ?action=edit
+	r.HandleFunc("/admin/articles/{slug}", s.AdminUpdateArticleGetHandler).Methods("GET")
+	r.HandleFunc("/admin/articles/{slug}", s.AdminUpdateArticleHandler).Methods("PUT")
+
+	r.HandleFunc("/admin/articles/{slug}", s.AdminDeleteArticleHandler).Methods("DELETE")
+	return r
+}
