5287ea79e2f6bdd7ad88c314a8ee87542f1b6c91
minhhoccode111 <minhhoccode111@gmail.com>
2025-09-12 19:58:01 +0700

    add: save + load functions

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,11 +1,16 @@
 package server
 
 import (
+	"bufio"
 	"encoding/base64"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
+	"text/template"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -15,21 +20,69 @@ type Page struct {
 	Title   string
 	Slug    string
 	Body    []byte
-	PubDate time.Time // 2006-01-02 15:04
+	PubDate time.Time
 }
+
+const timeFormat = "2006-01-02"
+
+var templates = template.Must(template.ParseGlob("template/*.html"))
+var linkRegex = regexp.MustCompile(`\[(.*?)\]`)
 
 func (p *Page) save() error {
 	filename := "data/" + p.Slug + ".md"
-	return os.WriteFile(filename, []byte(p.Body), 0600)
+	var body []byte
+	metadata := []string{
+		"---",
+		p.Title,
+		p.PubDate.Format(timeFormat),
+		"---",
+	}
+
+	body = append(body, []byte(strings.Join(metadata, "\n")+"\n")...)
+	body = append(body, p.Body...)
+
+	return os.WriteFile(filename, body, 0600)
 }
 
-func loadPage(title string) (*Page, error) {
-	filename := "data/" + title + ".txt"
-	body, err := os.ReadFile(filename)
+func loadPage(slug string) (*Page, error) {
+	filename := "data/" + slug + ".md"
+	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
-	return &Page{Title: title, Body: body}, nil
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	var title string
+	var pubDate time.Time
+
+	// read the first 4 lines
+	for range 4 {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			return nil, fmt.Errorf("%w", err)
+		}
+		if line == "---" {
+			continue
+		}
+		if title == "" {
+			title = line
+			continue
+		}
+
+		lineTime, err := time.Parse(timeFormat, line)
+		if err != nil {
+			return nil, fmt.Errorf("%w", err)
+		}
+		pubDate = lineTime
+	}
+
+	rest, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+
+	return &Page{title, slug, rest, pubDate}, nil
 }
 
 func unauthorizedResponse(w http.ResponseWriter) {
